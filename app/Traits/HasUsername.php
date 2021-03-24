3acad2aaf9b3e3ec895378a8ed1456de9e900f82
Pavel <ozpavel@yandex.ru>
2021-03-24 14:04:15 +0300

    fix: unique username

--- app/Traits/HasUsername.php
+++ app/Traits/HasUsername.php
@@ -2,12 +2,24 @@
 
 namespace App\Traits;
 
+use App\Models\User;
+
 trait HasUsername
 {
     public function getUsernameFromEmail($email): string
     {
-        $username = explode('@', $email);
+        $username = explode('@', $email)[0];
 
-        return $username[0];
+        if (User::where('username', $username)->first()) {
+            $num = 2;
+
+            while (User::where('username', $username . $num)->first()) {
+                $num++;
+            }
+
+            $username .= $num;
+        }
+
+        return $username;
     }
 }
