e5f114ff47016963b50e2de6fb444346da8655e2
Pavel <ozpavel@yandex.ru>
2021-03-26 02:06:25 +0300

    [CRM-48] feat: errors in russian

---
+++ resources/lang/ru/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'Ваш пароль был успешно сброшен!',
+    'sent' => 'Ссылка для сброса пароля отправлена на ваш адрес электронной почты.',
+    'throttled' => 'Пожалуйста, подождите, прежде чем пробовать снова.',
+    'token' => 'Некорректный токен для сброса пароля.',
+    'user' => "К сожалению, мы не можем найти пользователя с таким адресом электронной почты.",
+
+];
