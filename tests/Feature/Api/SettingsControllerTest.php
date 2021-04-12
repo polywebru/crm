d694ec7298eee0fa8ea7b9d7d7e00085d2b0f28f
Pavel <ozpavel@yandex.ru>
2021-04-12 21:34:41 +0300

    [CRM-77] feat: tests for settings

---
+++ tests/Feature/Api/SettingsControllerTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Feature\Api;
+
+use App\Models\Link;
+use App\Models\User;
+use Tests\TestCase;
+
+class SettingsControllerTest extends TestCase
+{
+    public function testInvoke()
+    {
+        $user = User::factory()->create();
+
+        $response = $this->actingAs($user)->get('api/v1/settings');
+
+        $response->assertStatus(200);
+        $response->assertJson([
+            'data' => [
+                'user_statuses' => User::STATUSES,
+                'user_study_durations' => User::STUDY_DURATIONS,
+                'link_types' => Link::TYPES,
+            ]
+        ]);
+    }
+}
