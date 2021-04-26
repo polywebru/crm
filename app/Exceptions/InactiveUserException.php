b1210f42ac6e2e7a08e809a7027ed4ccd767a110
Pavel <ozpavel@yandex.ru>
2021-04-27 01:15:02 +0300

    fix: inactive user exception structure

--- app/Exceptions/InactiveUserException.php
+++ app/Exceptions/InactiveUserException.php
@@ -2,16 +2,36 @@
 
 namespace App\Exceptions;
 
+use App\Models\User;
 use Illuminate\Http\Response;
+use Throwable;
 
 class InactiveUserException extends BaseException
 {
+    private User $user;
+
+    public function __construct(User $user, $message = "", $code = 0, Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+
+        $this->user = $user;
+    }
+
     public function render(string $message = null, $errorCode = 500, int $statusCode = null)
     {
-        return parent::render(
-            'Пользователь неактивен.',
-            Response::HTTP_FORBIDDEN . '.inactive',
-            Response::HTTP_FORBIDDEN
-        );
+        $data = [
+            'status' => false,
+            'error' => [
+                'code' => Response::HTTP_FORBIDDEN . '.inactive',
+                'message' => 'Пользователь неактивен.',
+            ],
+            'user' => [
+                'username' => $this->user->username,
+                'last_name' => $this->user->last_name,
+                'first_name' => $this->user->first_name,
+            ],
+        ];
+
+        return response($data, Response::HTTP_FORBIDDEN);
     }
 }
